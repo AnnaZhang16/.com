fe345051b9b73a0aafd06361eed18c14180751ef
AnnaZhang16 <anna.jang16@gmail.com>
2020-09-13 00:57:02 -0500

    added menu 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueSimpleContextMenu from "vue-simple-context-menu";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    components: {
+        "vue-simple-context-menu": VueSimpleContextMenu,
+    },
+    render: (h) => h(App),
+}).$mount("#app");
